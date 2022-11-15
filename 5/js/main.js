316703fecbf7724a62e60f82e2dd475759849317
Keks <keks@htmlacademy.ru>
2022-11-15 19:14:54 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,7 @@
+import {getRandomPositiveNumber, isAcceptableLength} from './util.js';
+import {renderPhoto} from './render-photo.js';
+
+getRandomPositiveNumber();
+isAcceptableLength();
+renderPhoto();
+
