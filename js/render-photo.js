3170d28856d4495a8fb9313066ec8827d3f6909c
Miracle112 <77540450+Miracle112@users.noreply.github.com>
2022-11-15 19:11:06 +0300

    Отрисуй меня полностью (часть 1)

--- js/render-photo.js
+++ js/render-photo.js
@@ -3,20 +3,19 @@ import {createOffersMock} from './data.js';
 const data = createOffersMock();
 
 function renderPhoto() {
-    const template = document.querySelector('#picture').content.querySelector('.picture');
-    const fragment = document.createDocumentFragment();
+  const template = document.querySelector('#picture').content.querySelector('.picture');
+  const fragment = document.createDocumentFragment();
 
-    data.forEach(({url, descriptions, likes, comments}) => {
-        const newPhoto = template.cloneNode(true);
-        newPhoto.querySelector('.picture__img').src = url;
-        newPhoto.querySelector('.picture__img').alt = descriptions;
-        newPhoto.querySelector('.picture__comments').textContent = comments;
-        newPhoto.querySelector('.picture__likes').textContent = likes;
-        fragment.appendChild(newPhoto);
-        console.log(newPhoto)
-    });
+  data.forEach(({url, descriptions, likes, comments}) => {
+    const newPhoto = template.cloneNode(true);
+    newPhoto.querySelector('.picture__img').src = url;
+    newPhoto.querySelector('.picture__img').alt = descriptions;
+    newPhoto.querySelector('.picture__comments').textContent = comments;
+    newPhoto.querySelector('.picture__likes').textContent = likes;
+    fragment.appendChild(newPhoto);
+  });
 
-    document.querySelector('.pictures').appendChild(fragment);
+  document.querySelector('.pictures').appendChild(fragment);
 };
 
 renderPhoto();
