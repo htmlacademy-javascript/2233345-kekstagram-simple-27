54ed0db20f719dd0e95486fde67b44b413e4da4f
Miracle112 <77540450+Miracle112@users.noreply.github.com>
2022-11-24 14:57:09 +0300

    refactoring

--- js/api.js
+++ js/api.js
@@ -1,34 +1,32 @@
 const getData = (onSuccess, onError) => {
-    fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-      .then((response) => {
-        if (response.ok) {
-          return response;
-        }
-        throw new Error(`${response.status} - ${response.statusText}`);
-  
-      })
-      .then((response) => response.json())
-      .then((data) => onSuccess(data))
-      .catch(() => onError('Произошла ошибка. Попробуйте повторить позже.'));
-  };
-  
-  const sendData = (onSuccess, onError, body) => {
-    fetch('https://27.javascript.pages.academy/kekstagram-simple',
-      {
-        method: 'POST',
-        body,
-      },
-    )
-      .then((response) => {
-        if (response.ok) {
-          onSuccess();
-        } else {
-          onError();
-        }
-      })
-      .catch(() => {
+  fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      throw new Error(`${response.status} - ${response.statusText}`);
+
+    })
+    .then((response) => response.json())
+    .then((data) => onSuccess(data))
+    .catch(() => onError('Произошла ошибка. Попробуйте повторить позже.'));
+};
+
+const sendData = (onSuccess, onError, body) => {
+  fetch('https://27.javascript.pages.academy/kekstagram-simple', {
+      method: 'POST',
+      body,
+    }, )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
         onError();
-      });
-  };
-  
-  export { getData, sendData };
+      }
+    })
+    .catch(() => {
+      onError();
+    });
+};
+
+export {getData, sendData};
