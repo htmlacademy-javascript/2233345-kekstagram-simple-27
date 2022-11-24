4f1ad34f4a1c1f898e046bb2d114ff519c657f36
Miracle112 <77540450+Miracle112@users.noreply.github.com>
2022-11-24 13:08:42 +0300

    module9-task1

--- js/reference.js
+++ js/reference.js
@@ -276,3 +276,80 @@ orderForm.addEventListener('submit', (evt) => {
   pristine.validate();
 });
 */
+
+// Подключение карты с помощью библиотеки Leaflet
+
+/*
+const nextButton = document.querySelector('#next');
+const map = L.map('map')
+  .setView({
+    lat: 59.95830,
+    lng: 30.31748,
+  }, 13);
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+  },
+).addTo(map);
+const points = [
+  {
+    title: 'Футура',
+    lat: 59.96925,
+    lng: 30.31730,
+  },
+  {
+    title: 'Шаверма',
+    lat: 59.96783,
+    lng: 30.31258,
+  },
+  {
+    title: 'Франк',
+    lat: 59.95958,
+    lng: 30.30228,
+  },
+  {
+    title: 'Ginza',
+    lat: 59.97292,
+    lng: 30.31982,
+  },
+];
+const icon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+const createCustomPopup = (point) => {
+  const balloonTemplate = document.querySelector('#balloon').content.querySelector('.balloon');
+  const popupElement = balloonTemplate.cloneNode(true);
+  popupElement.querySelector('.balloon__title').textContent = point.title;
+  popupElement.querySelector('.balloon__lat-lng').textContent = `Координаты: ${point.lat}, ${point.lng}`;
+  return popupElement;
+};
+const markerGroup = L.layerGroup().addTo(map);
+const createMarker = (point) => {
+  const {lat, lng} = point;
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      icon,
+    },
+  );
+  marker
+    .addTo(markerGroup)
+    .bindPopup(createCustomPopup(point));
+};
+points.slice(0, points.length / 2).forEach((point) => {
+  createMarker(point);
+});
+nextButton.addEventListener('click', () => {
+  markerGroup.clearLayers();
+  points.slice(points.length / 2).forEach((point) => {
+    createMarker(point);
+  });
+  nextButton.remove();
+});
+*/
