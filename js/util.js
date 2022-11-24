035839009061a2027abda5eb84f4e6fa83a45489
Miracle112 <77540450+Miracle112@users.noreply.github.com>
2022-11-24 12:55:13 +0300

    module8-task1

--- js/util.js
+++ js/util.js
@@ -1,28 +1,31 @@
 // isNaN() - функция, определяющая не число. Отдает булевый признак
 function getRandomPositiveInteger(a, b) {
 
-    if (a < 0 || b < 0) {
-      return NaN;
-    }
-  
-    const lower = Math.ceil(Math.min(a, b));
-    const upper = Math.floor(Math.max(a, b));
-  
-    const result = Math.random() * (upper - lower + 1) + lower;
-  
-    return Math.floor(result);
+  if (a < 0 || b < 0) {
+    return NaN;
   }
-  
-  function getRandomElement(elements) {
-    if (elements.length === 0) {
-      return undefined;
-    }
-    return elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+function getRandomElement(elements) {
+  if (elements.length === 0) {
+    return undefined;
   }
-  
-  function isEscapeKey(evt) {
-    return evt.key === 'Escape';
-  }
-  
-  export { getRandomElement, getRandomPositiveInteger, isEscapeKey };
-  
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+function isEscapeKey(evt) {
+  return evt.key === 'Escape';
+}
+
+export {
+  getRandomElement,
+  getRandomPositiveInteger,
+  isEscapeKey
+};
